92de37a9cb5862cd089d26ecfd2eea027422acb6
Javier Londono <javierlondono@gmail.com>
2024-12-12 22:20:33 +0100

    Update validation functions to include dataframe name as parameter, so when logger write message we can identified each.

--- src/validations.py
+++ src/validations.py
@@ -5,7 +5,7 @@ from pyspark.sql.functions import col
 logger = get_logger(__name__)
 
 
-def check_missing_values(df: DataFrame, column: str) -> int:
+def check_missing_values(df_name: str, df: DataFrame, column: str) -> int:
     """
     Checks for missing or null values in a specific column of the DataFrame.
 
@@ -18,14 +18,14 @@ def check_missing_values(df: DataFrame, column: str) -> int:
     """
     try:
         missing_count = df.filter(col(column).isNull()).count()
-        logger.info(f"Column '{column}' has {missing_count} missing or null values.")
+        logger.info(f"Dataframe: {df_name} Column '{column}' has {missing_count} missing or null values.")
         return missing_count
 
     except Exception as e:
         logger.error(f"Application encountered an error: {e}")
 
 
-def check_data_format_inconsistencies(df: DataFrame, column: str, expected_type: str) -> int:
+def check_data_format_inconsistencies(df_name: str, df: DataFrame, column: str, expected_type: str) -> int:
     """
     Checks for inconsistencies in the data format of a column.
 
@@ -37,6 +37,9 @@ def check_data_format_inconsistencies(df: DataFrame, column: str, expected_type:
     Returns:
         int: The count of inconsistent rows.
     """
-    inconsistent_count = df.filter(~col(column).cast(expected_type).isNotNull()).count()
-    logger.info(f"Column '{column}' has {inconsistent_count} inconsistent rows (not {expected_type}).")
-    return inconsistent_count
+    try:
+        inconsistent_count = df.filter(~col(column).cast(expected_type).isNotNull()).count()
+        logger.info(f"DataFrame: {df_name} Column '{column}' has {inconsistent_count} inconsistent rows (not {expected_type}).")
+        return inconsistent_count
+    except Exception as e:
+        logger.error(f"Application encountered an error: {e}")
