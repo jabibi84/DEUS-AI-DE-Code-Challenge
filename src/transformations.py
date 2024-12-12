e7c358fcca9e0b4c5715705c050af43ee275a0a4
Javier Londono <javierlondono@gmail.com>
2024-12-12 22:27:16 +0100

    Update categorize_price UDF to transform column value based on rule.

--- src/transformations.py
+++ src/transformations.py
@@ -2,13 +2,13 @@ from pyspark.sql import DataFrame
 from pyspark.sql.functions import col, when
 
 
-def add_status_column(df: DataFrame) -> DataFrame:
-    """
-    Agrega una columna 'status' basada en la columna 'value':
-    - 'high' si value > 50
-    - 'low' si value <= 50
-    """
-    return df.withColumn(
-        "status",
-        when(col("value") > 50, "high").otherwise("low")
-    )
+# Define the categorization function
+def categorize_price(price):
+    if price < 20:
+        return "Low"
+    elif 20 <= price <= 100:
+        return "Medium"
+    elif price > 100:
+        return "High"
+    else:
+        return None
