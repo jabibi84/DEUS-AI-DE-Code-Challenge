b88c547517942731c8bd8829eb71a51fda54a882
Javier Londono <javierlondono@gmail.com>
2024-12-12 22:14:39 +0100

    Update utils function to get datatype from a spark dataframe and column.

--- src/utils.py
+++ src/utils.py
@@ -26,3 +26,37 @@ def get_logger(name: str) -> logging.Logger:
         logger.addHandler(console_handler)
 
     return logger
+
+
+def get_dtype(df,colname):
+    return [dtype for name, dtype in df.dtypes if name == colname][0]
+
+
+logger = get_logger(__name__)
+
+
+def get_config(file_path: str) -> dict:
+    """
+    Reads a configuration file in JSON format and parses it into a dictionary.
+
+    Args:
+        file_path (str): Path to the JSON configuration file.
+
+    Returns:
+        dict: Parsed configuration as a dictionary.
+    """
+    logger.info("Getting Dataset configuration.")
+    try:
+        with open(file_path, 'r') as file:
+            config = json.load(file)
+        logger.info("Configuration File loaded successfully.")
+        return config
+
+    except FileNotFoundError:
+        logger.error(f"Error: Configuration file not found at {file_path}")
+        raise
+    except json.JSONDecodeError as e:
+        logger.error(f"Error: Failed to decode JSON. {e}")
+        raise
+    except Exception as e:
+        logger.error(f"Error: Failed to decode JSON. {e}")
