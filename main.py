4d6ee0720e590750e69b269a5156f1332c2ea58e
Javier Londono <javierlondono@gmail.com>
2024-12-13 19:31:09 +0100

    Update main file to reference module functions and execute operations requested in challenge.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import sys
 
 sys.path.append("./src")
-
+from pyspark.sql.functions import udf, cast
+from pyspark.sql.types import StringType, FloatType
+from src.SparkSchemas import SchemaManager
 from src.proccesing import load_data
+from src.validations import check_missing_values, check_data_format, check_duplicates
+from src.cleanning import (
+    standardize_date_format,
+    drop_duplicates_by_column,
+    enforce_dataframe_schema,
+)
+from src.transformations import (
+    calculate_total_revenue,
+    calculate_monthly_sales,
+    enrich_data,
+    categorize_price,
+    write_dataframe,
+)
 from pyspark.sql import SparkSession
-from src.utils import get_logger
+from src.utils import get_logger, get_config
 
 logger = get_logger(__name__)
 
@@ -17,14 +32,71 @@ def main():
         ).getOrCreate()
         logger.info("SparkSession initialized.")
 
-        sales_df = load_data(spark, "/app/data/sales_uuid.csv")
-        logger.info("sales.csv Loaded Success..")
+        # get configuration
+        config = get_config("./config/metadata.json")
+        for tbl in config["datasets"]:
+            name = tbl["name"]
+            path = tbl["file"]
+            columns = tbl["columns"]
+            schema = SchemaManager.get_schema(name)
+            # logger.info(schema)
 
-        products_df = load_data(spark, "/app/data/products_uuid.csv")
-        logger.info("Products.csv Loaded Success..")
+            # load datasets and validate datasets
+            df_data = load_data(spark, path)
+            globals()[f"df_{name}"] = load_data(spark, path)
+            logger.info(f"Loaded dataset: {name}  path: {path}")
+            check_duplicates(name, df_data)
+            for field in columns:
+                check_missing_values(name, df_data, field["name"])
+                check_data_format(name, df_data, field["name"], field["type"])
+                if field.get("unique"):
+                    check_duplicates(name, df_data, field["name"])
 
-        stores_df = load_data(spark, "/app/data/stores_uuid.csv")
-        logger.info("stores.csv Loaded Success..")
+        # data cleanning
+        df_transactions = standardize_date_format(
+            df_SalesTransactions, "transaction_date"
+        ).drop("transaction_date")
+        df_transactions = df_transactions.withColumnRenamed(
+            "standardized_date", "transaction_date"
+        )
+
+        # Registrar la función como UDF (User Defined Function)
+        categorize_price_udf = udf(categorize_price, StringType())
+        df_transactions = df_transactions.withColumn(
+            "price_category",
+            categorize_price_udf(df_transactions["price"].cast(FloatType())),
+        )
+        df_transactions.show()
+
+        # transformations
+        df_revenue = calculate_total_revenue(df_transactions, df_Products)
+
+        df_monthly_sales = calculate_monthly_sales(df_transactions, df_Products)
+
+        df_enrich = enrich_data(df_transactions, df_Products, df_Stores)
+
+        # output
+        write_dataframe(
+            df=df_enrich,
+            output_path="data/output/sales_product_store",
+            format="parquet",
+            partition_by=["category", "transaction_date"],
+            mode="overwrite",
+        )
+
+        write_dataframe(
+            df=df_revenue,
+            output_path="data/output/Revenueinsights",
+            format="csv",
+            mode="overwrite",
+        )
+
+        write_dataframe(
+            df=df_transactions,
+            output_path="data/output/transactions",
+            format="csv",
+            mode="overwrite",
+        )
 
     except Exception as e:
         logger.error(f"Application encountered an error: {e}")
