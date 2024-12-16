3465c20949d0d9e75ecc6ec63d4a3173e0bbac9f
Javier Londono <javierlondono@gmail.com>
2024-12-16 10:58:11 +0100

    Update code formating as black recommendations.

--- src/proccesing.py
+++ src/proccesing.py
@@ -2,6 +2,9 @@ import os
 from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.types import StructType
 from pyspark.sql.utils import AnalysisException
+from src.utils import get_logger
+
+logger = get_logger(__name__)
 
 
 def load_csv(spark: SparkSession, path: str, schema: StructType = None) -> DataFrame:
@@ -23,26 +26,39 @@ def load_csv(spark: SparkSession, path: str, schema: StructType = None) -> DataF
     """
     # Validate input types
     if not isinstance(spark, SparkSession):
+        logger.error("The 'spark' parameter must be a valid SparkSession.")
         raise TypeError("The 'spark' parameter must be a valid SparkSession.")
     if not isinstance(path, str):
+        logger.error("The 'path' parameter must be a string.")
         raise TypeError("The 'path' parameter must be a string.")
     if schema is not None and not isinstance(schema, StructType):
+        logger.error("The 'schema' parameter must be of type StructType or None.")
         raise TypeError("The 'schema' parameter must be of type StructType or None.")
 
     # Check if the file path exists
     if not os.path.exists(path):
+        logger.error(f"The specified path '{path}' does not exist.")
         raise ValueError(f"The specified path '{path}' does not exist.")
 
     # Check if the file has a .csv extension
     if not path.lower().endswith(".csv"):
+        logger.error(f"The specified file '{path}' is not a CSV file.")
         raise ValueError(f"The specified file '{path}' is not a CSV file.")
 
     try:
         # Load the CSV file with or without schema
         if schema:
-            df = spark.read.format("csv").option("header", "true").schema(schema).load(path)
+            df = (
+                spark.read.format("csv")
+                .option("header", "true")
+                .schema(schema)
+                .load(path)
+            )
         else:
             df = spark.read.format("csv").option("header", "true").load(path)
+
+        logger.info(f"Successfully loaded CSV file from path: {path}")
         return df
     except AnalysisException as e:
+        logger.error(f"Failed to load CSV file from path '{path}'. Error: {e}")
         raise ValueError(f"Failed to load CSV file from path '{path}'. Error: {e}")
