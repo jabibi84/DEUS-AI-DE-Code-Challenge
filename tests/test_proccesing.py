82344612d5312643d032ea3ae0349de3e90dde11
Javier Londono <javierlondono@gmail.com>
2024-12-27 11:24:16 +0100

    Update test_proccesing function with properly parameters adapted to specifics functions.

--- tests/test_proccesing.py
+++ tests/test_proccesing.py
@@ -1,11 +1,16 @@
-from pyspark.sql import SparkSession
+import sys
+import os
+import pytest
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from chispa.dataframe_comparer import assert_df_equality
-from src.proccesing import load_csv
+
+# Add src to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+from src.proccesing import load_csv  # Import your custom function
 
 
 def test_load_csv(spark):
-    csv_path = "tests/sample_data.csv"
+    csv_path = "tests/sample_data.csv"  # Update the path to the test CSV file
     expected_data = [("Alice", 30), ("Bob", 25)]
     schema = StructType(
         [
@@ -14,7 +19,7 @@ def test_load_csv(spark):
         ]
     )
     expected_df = spark.createDataFrame(expected_data, schema)
-    result = load_csv(spark, csv_path)
+    result = load_csv(spark, csv_path, schema)
     assert_df_equality(
         result, expected_df, ignore_row_order=True, ignore_column_order=True
     )
